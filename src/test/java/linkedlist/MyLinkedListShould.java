86f011945e164917405749c47871a3838299a8d9
Alvaro Garcia <derecerca@gmail.com>
2015-02-04 22:26:31 +0100

    refactor: extract @before

--- src/test/java/linkedlist/MyLinkedListShould.java
+++ src/test/java/linkedlist/MyLinkedListShould.java
@@ -3,13 +3,20 @@ package linkedlist;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyLinkedListShould {
 
+	private MyLinkedList sut;
+
+	@Before
+	public void setUp() {
+		sut = new MyLinkedList();
+	}
+
 	@Test
 	public void be_empty_by_default() {
-		final MyLinkedList sut = new MyLinkedList();
 		assertThat(sut.size(), is(0));
 	}
 }
