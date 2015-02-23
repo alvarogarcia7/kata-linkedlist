ca9c2f252b646f3c4ebcff1ab7d25c91016e7ca9
Alvaro Garcia <derecerca@gmail.com>
2015-02-23 22:55:00 +0100

    green

--- src/main/java/linkedlist/PersistentList.java
+++ src/main/java/linkedlist/PersistentList.java
@@ -2,6 +2,7 @@ package linkedlist;
 
 public class PersistentList {
 
+	private String value;
 	private int size = 0;
 
 	public int size() {
@@ -11,11 +12,15 @@ public class PersistentList {
 	public PersistentList add(final String string) {
 		final PersistentList result = new PersistentList();
 		result.size = size + 1;
+		result.value = string;
 		return result;
 	}
 
 	public boolean contains(final String element) {
-		return size > 0;
+		if (size == 0) {
+			return false;
+		}
+		return value.equals(element);
 	}
 
 }
